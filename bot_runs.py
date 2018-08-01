f9ca5ee2d1657a920acf094d5571c8d62348a1af
kemalcan bora <kemalcanbora@gmail.com>
2018-08-01 23:45:25 +0300

    Update bot_runs.py

--- bot_runs.py
+++ bot_runs.py
@@ -1,7 +1,6 @@
 from bot_modacruz.modacruz_web_scraping import ModaCruzBot
 from bot_zara.zara_web_scraping import ZaraBot
 from threading import Thread
-from ML import FeatureExtractor
 
 def moda_cruz_bot():
     ModaCruzBot().run(save_path="/static/img/")
@@ -9,8 +8,7 @@ def moda_cruz_bot():
 def zara_bot():
     ZaraBot().run("/static/img/")
 
-FeatureExtractor().get_all_imgs_feature()
-#
-# if __name__ == '__main__':
-#     cruz_thread = Thread(target = moda_cruz_bot).start()
-#     zara_thread = Thread(target = zara_bot).start()
+
+if __name__ == '__main__':
+     cruz_thread = Thread(target = moda_cruz_bot).start()
+     zara_thread = Thread(target = zara_bot).start()
